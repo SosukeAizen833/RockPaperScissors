f64e5a4fa30d6674044b97de6a15ef271e0e35c3
SosukeAizen833 <Spandan833@gmail.com>
2021-10-17 21:15:55 +0530

    single match game. results not styled

--- scripts/script.js
+++ scripts/script.js
@@ -7,13 +7,14 @@ const paper_button = document.createElement('button')
 const scissor_button  = document.createElement('button')
 
 rock_button.classList.add('btn-choice')
-rock_button.id = "rock"
+rock_button.setAttribute('id','rock')
 paper_button.classList.add('btn-choice')
-paper_button.id = "paper"
+paper_button.setAttribute('id','paper')
 scissor_button.classList.add('btn-choice')
-scissor_button.id = "scissors"
+scissor_button.setAttribute('id','scissor')
 
 console.log(paper_button.id)
+console.log(rock_button.id)
 
 const rock_img = document.createElement('img')
 rock_img.src="../images/8-2-rock-png-picture-thumb.png"
@@ -33,98 +34,117 @@ content.appendChild(paper_button)
 content.appendChild(scissor_button)
 
 const body = document.body
-// function get_comp_choice(){
-//     let choice  = Math.floor(Math.random()*3 + 1)
-//     if(choice === 1){
-//         console.log("Computer chose: rock")
-//         return "rock"
-//     }
-//     if(choice === 2){
-//         console.log("Computer chose: paper")
-//         return "paper"
-//     }
-//     if(choice === 3){
-//         console.log("Computer chose: scissors")
-//         return "scissors"
-//     }
-// }
+
+let player_score = 0;
+let comp_score = 0;
+
+function get_comp_choice(){
+    let choice  = Math.floor(Math.random()*3 + 1)
+    if(choice === 1){
+        console.log("Computer chose: rock")
+        return "rock"
+    }
+    if(choice === 2){
+        console.log("Computer chose: paper")
+        return "paper"
+    }
+    if(choice === 3){
+        console.log("Computer chose: scissors")
+        return "scissor"
+    }
+}
 
 
 
-// function start_game(rounds){
-//     let player_score  = 0;
-//     let comp_score = 0;
-//     for(let i=0;i<rounds;i++){
+function start_game(player_choice){
          
-//         let player_choice = get_player_choice();
-//         let comp_choice = get_comp_choice();
+        
+        let comp_choice = get_comp_choice();
 
-//         if(comp_choice==player_choice){
-//             console.log("match tied")
-//             display_score(player_score,comp_score)
-//             continue;
-//         }
 
-//         if(comp_choice==='rock'){
-//             if(player_choice==='scissors'){
-//                 comp_score++;
-//                 console.log("You lose this round")
-//             }else{
-//                 player_score++;
-//                 console.log("You win this round")
-//             }
+        if(comp_choice==player_choice){
             
-//         }
-//         else if(comp_choice==='paper'){
-//             if(player_choice === 'scissors'){
-//                 player_score++;
-//                 console.log("You win this round")
-//             }else{
-//                 comp_score++;
-//                 console.log("You lose this round")
-//             }
+            winner(player_score,comp_score)
+            return;
+        }
+
+        if(comp_choice==='rock'){
+            if(player_choice==='scissor'){
+                comp_score++;
+                console.log("You lose this round")
+            }else{
+                player_score++;
+                console.log("You win this round")
+            }
+            winner(player_score,comp_score)
             
-//         }
-//         else{
-//             if(player_choice==='rock'){
-//                 player_score++;
-//                 console.log("You win this round")
-//             }else{
-//                 comp_score++;
-//                 console.log('You lose this round')
-//             }
-//         }
+        }
+        else if(comp_choice==='paper'){
+            if(player_choice === 'scissor'){
+                player_score++;
+                console.log("You win this round")
+            }else{
+                comp_score++;
+                console.log("You lose this round")
+            }
+            winner(player_score,comp_score)
+            
+        }
+        else{
+            if(player_choice==='rock'){
+                player_score++;
+                console.log("You win this round")
+            }else{
+                comp_score++;
+                console.log('You lose this round')
+            }
+            winner(player_score,comp_score)
+        }
 
-//         display_score(player_score,comp_score)
-//     }
+        player_score = 0;
+        comp_score = 0;
+        return;
+        display_score(player_score,comp_score)
 
-//     winner(player_score,comp_score)
-// }
+    
+}
 
 
-// function winner(player_score,comp_score){
-//     if(comp_score>player_score){
-//         alert("You lose")
-//     }
-//     else if(comp_score < player_score){
-//         alert("You win")
-//     }
-//     else{
-//         alert("Drawn")
-//     }
-// }
+function winner(player_score,comp_score){
 
-// function display_score(player_score,comp_score){
-//     console.log(`Player Score: ${player_score}`)
-//     console.log(`Computer Score: ${comp_score}`)    
-// }
+    const div = document.createElement('div')
+    if(comp_score>player_score){
+        winner.innerHTML = "You lose"
+    }
+    else if(comp_score < player_score){
+        winner.innerHTML = "You Win"
+    }
+    else{
+        winner.innerHTML = "Drawn"
+    }
+    let body = document.body
+    body.appendChild(winner)
+}
+
+function display_score(player_score,comp_score){
+    const div1 = document.createElement('div')
+    div1.innerHTML=`Player Score: ${player_score}`
+    console.log(`Player Score: ${player_score}`)
+    const div2 = document.createElement('div')
+    div2.innerHTML=`Computer Score: ${comp_score}`
+    console.log(`Computer Score: ${comp_score}`)    
+}
 
 
 startBtn.addEventListener('click',()=>{
     startBtn.remove()
     body.appendChild(content)
+    const buttons = document.getElementsByClassName('btn-choice')
+    let a  = Array.from(buttons)
+    for(let i=0;i<a.length;i++){
+        a[i].addEventListener('click',()=>{
+            start_game(a[i].id)
+        })
+    }
 })
 
-rock_button.addEventListener('click',()=>{
-    start_game(rock_button.id)
-})
